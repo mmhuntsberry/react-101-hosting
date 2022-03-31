9098365e281084a6bd16e2db2b4ffa47dd734336
matt.huntsberry <matt.huntsberry@nielsen.com>
2022-03-30 18:42:24 -0700

    chore: initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Button from "./components/Button";
+import "./app.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>{count}</h1>
+      <Button text="Decrement" handleClick={() => setCount(count - 1)} />
+      <Button text="Increment" handleClick={() => setCount(count + 1)} />
+      <Button text="Reset Me" handleClick={() => setCount(0)} />
+    </main>
   );
 }
 
